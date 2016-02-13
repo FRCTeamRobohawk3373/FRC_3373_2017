d063d6197f2db6ad84f19c5627e0bc84f9085bcb
Alex Iasso <RoboHawks@10.0.28.28>
2016-02-13 11:42:34 -0500

    Removed an error

--- 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkSuperDualMotor.java
+++ 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkSuperDualMotor.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team3373.robot;
 public class HawkSuperDualMotor extends HawkSuperMotor{
 
 	public HawkSuperDualMotor(int deviceNumber, int encoderMin, int encoderMax,int deviceNumber2, int encoderMin2, int encoderMax2 ) {
-		super(deviceNumber);
+		super(deviceNumber, encoderMin, encoderMax);
 		// TODO Auto-generated constructor stub
 	}
 	public void dualGoToHeight(double targetHeight){
