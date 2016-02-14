6dfc917f7e86a2b56a83bf62bddfab9719035289
RoboHawks <RoboHawks@10.0.28.76>
2016-02-14 17:58:20 -0500

    Created an class to limit actuator movement based on pot values

---
+++ 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkActuator.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team3373.robot;
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.AnalogInput;;
+
+public class HawkActuator extends HawkSuperMotor  {
+	double maxPotValue;
+	double minPotValue;
+	AnalogInput pot;
+	public HawkActuator(int actuatorID,double actuatorMaxPotValue ,double actuatorMinPotValue, double maxSpeedChange,int potID){
+		super(actuatorID,0, 0,0,0,0,maxSpeedChange);
+		maxPotValue = actuatorMaxPotValue;
+		minPotValue = actuatorMinPotValue;
+		pot = new AnalogInput(potID);
+	}
+	public void set(double speed){
+		System.out.println(pot.getValue());
+		if(pot.getValue() > maxPotValue){
+			super.set(-1);
+		}
+		if(pot.getValue() < minPotValue){
+			super.set(1);
+		}
+		else{
+			super.set(speed);
+		}
+	}
+}
