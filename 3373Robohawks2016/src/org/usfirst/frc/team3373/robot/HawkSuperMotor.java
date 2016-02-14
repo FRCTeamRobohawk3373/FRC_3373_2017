a8c9db6e4ceed0de2366d101b446feb1402038e4
Joey Dyer <RoboHawks@RoboComputer.lan>
2016-02-13 21:03:50 -0500

    Revised super motor with enhanced "set" and "getSpeed" methods

--- 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkSuperMotor.java
+++ 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkSuperMotor.java
@@ -9,14 +9,19 @@ public class HawkSuperMotor extends CANTalon {
 	double targetEncoderPos;
 	int maxPercentSpeed;
 	int minPercentSpeed;
-	public HawkSuperMotor(int deviceNumber, int encoderMin, int encoderMax, int maxPercent, int minPercent) {
+	double maxDelta;
+	double currentSpeed;
+	//max speed change = maximum speed change between iterations
+	public HawkSuperMotor(int deviceNumber, int encoderMin, int encoderMax, double maxSpeedChange) {
 		super(deviceNumber);
 		double encoderRange = encoderMax-encoderMin;
 		range = encoderRange;
 		rangeMin = encoderMin;
 		rangeMax = encoderMax;
-		maxPercentSpeed = maxPercent;
-		minPercentSpeed = minPercent;
+		maxDelta = maxSpeedChange;
+		currentSpeed = 0.0;
+		//maxPercentSpeed = maxPercent;
+		//minPercentSpeed = minPercent;
 	}
 	public void setScaled(double speed){
 		set(speed*(maxPercentSpeed/100));
@@ -48,6 +53,29 @@ public class HawkSuperMotor extends CANTalon {
 		}
 		return targetEncoderPos;
 		
+	} 
+	@Override
+	public void set(double speed){
+		//If the change in speed is greater than max delta, reduce the change to max delta
+		double currentDelta = Math.abs(currentSpeed - speed);
+		System.out.println(currentSpeed + " " + currentDelta);
+		if(currentDelta > maxDelta){
+			if(speed > currentSpeed){
+				speed = currentSpeed + maxDelta;
+			}
+			else{
+				speed = currentSpeed - maxDelta;
+			}
+		}
+		System.out.println("Speed:" + speed);
+		currentSpeed = speed;
+		super.set(speed);
+	}
+	public void resetCurrentSpeed(){
+		currentSpeed = 0.0;
+	}
+	public double getSpeed(){
+		return currentSpeed;
 	}
 
 }
