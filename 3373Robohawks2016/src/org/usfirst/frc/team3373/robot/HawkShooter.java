1d5d002f573104dbaa9d554f16d80a07eb7ea334
RoboHawks <RoboHawks@10.0.28.16>
2016-02-20 15:31:15 -0500

    Removed methods that need logic that would require threading

--- 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkShooter.java
+++ 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkShooter.java
@@ -1,28 +1,17 @@
 package org.usfirst.frc.team3373.robot;
 public class HawkShooter {
 	HawkSuperMotor shooterMain = new HawkSuperMotor(5,0,0,0,0,0,.1);
-	HawkSuperMotor shooterControl = new HawkSuperMotor(6,0,0,0,0,0,.1);
-	//HawkActuator shooterAimer = new HawkSuperMotor(14,0,0,0,0,0,.1);
+	HawkSuperMotor shooterControl = new HawkSuperMotor(6,0,0,0,0,0,1);
+	HawkActuator shooterAimer = new HawkActuator(14,0,0,.1); //still need to test for max and min pot values.
 	HawkVision hawkVision = new HawkVision();
-	private void goToAngle(double angle){
+	public void goToAngle(double angle){
 		//go to angle based off pot value
-	}
-	public void  aimShooter(){
-		//get distance from vision System
-		//calculate angle based on distance
-		//call go to angle to move to angle
-	}
-	public void shoot(){
-		//start up shooterMain motor to maximum speed
-		//run shooter control motor forward to release ball
-		
-	}
-		
+	}	
 	public void takeInBall(){
 		//angle down into pickup position
 		goToAngle(0);
 		//spin both motors backwards to pull in ball
-		if(shooterControl.isRevLimitSwitchClosed()){
+		if(!shooterControl.isRevLimitSwitchClosed()){
 		shooterMain.set(-.5);
 		shooterControl.set(-.5);
 		}
