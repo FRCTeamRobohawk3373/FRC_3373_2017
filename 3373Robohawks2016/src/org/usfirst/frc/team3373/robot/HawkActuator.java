f78f5db357066036c800404a1e08d17b69c203e7
Alex Iasso <Robohawks@10.0.28.36>
2016-02-29 16:27:23 -0500

    Added actuator potentiometer capabilities.

--- 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkActuator.java
+++ 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkActuator.java
@@ -6,17 +6,19 @@ public class HawkActuator extends HawkSuperMotor  {
 	double maxPotValue;
 	double minPotValue;
 	int targetPotPos;
+	double range;
 	public HawkActuator(int actuatorID,double actuatorMaxPotValue ,double actuatorMinPotValue, double maxSpeedChange, int limitSwitchForwID, int limitSwitchRevID){
 		super(actuatorID, 0, 0, 0, 0, 6,maxSpeedChange, 1, limitSwitchForwID ,limitSwitchRevID);
 		maxPotValue = actuatorMaxPotValue;
 		minPotValue = actuatorMinPotValue;
+		range = actuatorMaxPotValue-actuatorMinPotValue;
 	}
 	public void set(double speed){
 		System.out.println(getAnalogInRaw());
 		if(getAnalogInRaw() > maxPotValue){
 			super.set(-1);
 			}
-		if(getAnalogInRaw() < minPotValue){
+		else if(getAnalogInRaw() < minPotValue){
 			super.set(1);
 			}
 		else{
@@ -24,18 +26,18 @@ public class HawkActuator extends HawkSuperMotor  {
 			}
 	}
 	public double goToHeight(double targetHeight){
-		targetPotPos = (int) ((maxPotValue/6) * targetHeight);
-		if(getEncPosition()<rangeMin){                             //Prevents the motor from hitting or passing its lower limit
+		targetPotPos = (int) (((range/5.5) * targetHeight) + minPotValue);
+		if(getAnalogInRaw()<minPotValue){                             //Prevents the motor from hitting or passing its lower limit
 			setScaled(.1);
-		}else if(getEncPosition()>rangeMax){                       //Prevents the motor from hitting or passing its upper limit
+		}else if(getAnalogInRaw()>maxPotValue){                       //Prevents the motor from hitting or passing its upper limit
 			setScaled(-.1);
-		}else if(getEncPosition()>targetPotPos+3 && getEncPosition() <targetPotPos+5){
+		}else if(getAnalogInRaw()>targetPotPos+3 && getAnalogInRaw() <targetPotPos+5){
 			setScaled(-.1);
-		}else if(getEncPosition()>targetPotPos+3){
+		}else if(getAnalogInRaw()>targetPotPos+3){
 			setScaled(-.5);
-		}else if(getEncPosition()<targetPotPos-3 && getEncPosition() > targetPotPos-5){
+		}else if(getAnalogInRaw()<targetPotPos-3 && getAnalogInRaw() > targetPotPos-5){
 			setScaled(.1);
-		}else if(getEncPosition()< targetPotPos-3){
+		}else if(getAnalogInRaw()< targetPotPos-3){
 			setScaled(.5);
 		}else{
 			setScaled(0);
