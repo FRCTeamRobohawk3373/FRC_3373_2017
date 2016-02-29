529bbc2864117d09170407f5213bf438538669c2
Robohawks <Robohawks@10.0.28.36>
2016-02-28 19:29:46 -0500

    Added actuator goToHeight().

--- 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkActuator.java
+++ 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkActuator.java
@@ -5,8 +5,9 @@ import edu.wpi.first.wpilibj.AnalogInput;;
 public class HawkActuator extends HawkSuperMotor  {
 	double maxPotValue;
 	double minPotValue;
+	int targetPotPos;
 	public HawkActuator(int actuatorID,double actuatorMaxPotValue ,double actuatorMinPotValue, double maxSpeedChange, int limitSwitchForwID, int limitSwitchRevID){
-		super(actuatorID, 0, 0, 0, 0, 0,maxSpeedChange, 1, limitSwitchForwID ,limitSwitchRevID);
+		super(actuatorID, 0, 0, 0, 0, 6,maxSpeedChange, 1, limitSwitchForwID ,limitSwitchRevID);
 		maxPotValue = actuatorMaxPotValue;
 		minPotValue = actuatorMinPotValue;
 	}
@@ -21,6 +22,25 @@ public class HawkActuator extends HawkSuperMotor  {
 		else{
 			super.set(speed);
 			}
+	}
+	public double goToHeight(double targetHeight){
+		targetPotPos = (int) ((maxPotValue/6) * targetHeight);
+		if(getEncPosition()<rangeMin){                             //Prevents the motor from hitting or passing its lower limit
+			setScaled(.1);
+		}else if(getEncPosition()>rangeMax){                       //Prevents the motor from hitting or passing its upper limit
+			setScaled(-.1);
+		}else if(getEncPosition()>targetPotPos+3 && getEncPosition() <targetPotPos+5){
+			setScaled(-.1);
+		}else if(getEncPosition()>targetPotPos+3){
+			setScaled(-.5);
+		}else if(getEncPosition()<targetPotPos-3 && getEncPosition() > targetPotPos-5){
+			setScaled(.1);
+		}else if(getEncPosition()< targetPotPos-3){
+			setScaled(.5);
+		}else{
+			setScaled(0);
+		}
+		return targetHeight;
 	}
 }
 
