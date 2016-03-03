e04626e4c6776929e9a2b3491f99487b72854dbb
Alex Iasso <RoboHawks@10.0.28.39>
2016-03-03 17:33:07 -0500

    Created dual linear actuator synchronization code. Needs robot testing.

--- 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkActuator.java
+++ 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkActuator.java
@@ -7,8 +7,10 @@ public class HawkActuator extends HawkSuperMotor  {
 	double minPotValue;
 	int targetPotPos;
 	double range;
+	double currentActuatorHeight;
+	double targetHeight;
 	public HawkActuator(int actuatorID,double actuatorMaxPotValue ,double actuatorMinPotValue, double maxSpeedChange, int limitSwitchForwID, int limitSwitchRevID){
-		super(actuatorID, 0, 0, 0, 0, 0,maxSpeedChange, -1, limitSwitchForwID ,limitSwitchRevID);
+		super(actuatorID, 0, 0, 100, 0, 5.5625,maxSpeedChange, -1, limitSwitchForwID ,limitSwitchRevID);
 		maxPotValue = actuatorMaxPotValue;
 		minPotValue = actuatorMinPotValue;
 		range = actuatorMaxPotValue-actuatorMinPotValue;
@@ -16,33 +18,94 @@ public class HawkActuator extends HawkSuperMotor  {
 	public void set(double speed){
 		System.out.println(getAnalogInRaw());
 		if(getAnalogInRaw() > maxPotValue){
-			super.set(-1);
+			super.set(-.1);
 			}
 		else if(getAnalogInRaw() < minPotValue){
-			super.set(1);
+			super.set(.1);
 			}
 		else{
 			super.set(speed);
 			}
 	}
 	public double goToHeight(double targetHeight){
-		targetPotPos = (int) (((range/5.5625) * targetHeight) + minPotValue);
-		if(getAnalogInRaw()<minPotValue){                             //Prevents the motor from hitting or passing its lower limit
+		double range1 = range/5.625;
+		double range2 = range1 * targetHeight;
+		double range3 = range2 + minPotValue;
+		targetPotPos = (int) range3;
+		if(getAnalogInRaw()<minPotValue-10){                             //Prevents the motor from hitting or passing its lower limit
 			setScaled(.1);
-		}else if(getAnalogInRaw()>maxPotValue){                       //Prevents the motor from hitting or passing its upper limit
+			System.out.println("reached lower limit.");
+		}else if(getAnalogInRaw()>maxPotValue+10){                       //Prevents the motor from hitting or passing its upper limit
 			setScaled(-.1);
-		}else if(getAnalogInRaw()>targetPotPos+3 && getAnalogInRaw() <targetPotPos+5){
+			System.out.println("reached upper limit");
+		}else if(getAnalogInRaw()>targetPotPos+3 && getAnalogInRaw() <targetPotPos+25){
 			setScaled(-.1);
-		}else if(getAnalogInRaw()>targetPotPos+3){
-			setScaled(-.5);
-		}else if(getAnalogInRaw()<targetPotPos-3 && getAnalogInRaw() > targetPotPos-5){
-			setScaled(.1);
-		}else if(getAnalogInRaw()< targetPotPos-3){
+			System.out.println("slowing while going down");
+		}else if(getAnalogInRaw()>targetPotPos+25){
+			setScaled(-.25);
+			System.out.println("going down");
+		}else if(getAnalogInRaw()<targetPotPos-3 && getAnalogInRaw() > targetPotPos-25){
 			setScaled(.5);
+			System.out.println("Slowing whole going up.");
+		}else if(getAnalogInRaw()< targetPotPos-25){
+			setScaled(.25);
+			System.out.println("Going down.");
 		}else{
 			setScaled(0);
+			System.out.println("Stopping.");
 		}
-		return targetHeight;
+		currentHeight = (getAnalogInRaw() - minPotValue)/ range;
+		System.out.println("Target pot pos:" + targetPotPos);
+		System.out.println("range1: "+ range1);
+		System.out.println("range2: "+ range2);
+		System.out.println("range3: "+ range3);
+		targetHeight = targetHeight;
+		return targetPotPos;
+	}
+	public double sniperToHeight(double targetHeight){
+		double range1 = range/5.625;
+		double range2 = range1 * targetHeight;
+		double range3 = range2 + minPotValue;
+		targetPotPos = (int) range3;
+		if(getAnalogInRaw()<minPotValue-10){                             //Prevents the motor from hitting or passing its lower limit
+			setScaled(.1/2);
+			System.out.println("reached lower limit.");
+		}else if(getAnalogInRaw()>maxPotValue+10){                       //Prevents the motor from hitting or passing its upper limit
+			setScaled(-.1/2);
+			System.out.println("reached upper limit");
+		}else if(getAnalogInRaw()>targetPotPos+3 && getAnalogInRaw() <targetPotPos+25){
+			setScaled(-.1/2);
+			System.out.println("slowing while going down");
+		}else if(getAnalogInRaw()>targetPotPos+25){
+			setScaled(-.25/2);
+			System.out.println("going down");
+		}else if(getAnalogInRaw()<targetPotPos-3 && getAnalogInRaw() > targetPotPos-25){
+			setScaled(.5/2);
+			System.out.println("Slowing whole going up.");
+		}else if(getAnalogInRaw()< targetPotPos-25){
+			setScaled(.25/2);
+			System.out.println("Going down.");
+		}else{
+			setScaled(0/2);
+			System.out.println("Stopping.");
+		}
+		currentHeight = (getAnalogInRaw() - minPotValue)/ range;
+		System.out.println("Target pot pos:" + targetPotPos);
+		System.out.println("range1: "+ range1);
+		System.out.println("range2: "+ range2);
+		System.out.println("range3: "+ range3);
+		return targetPotPos;
+	}
+	public double getHeight(){
+		currentActuatorHeight = ((getAnalogInRaw() - minPotValue)/range) * 5.625;
+		return currentActuatorHeight;
+	}
+	public int getTargetPotPos(){
+		return targetPotPos;
+	}
+	public double getDistanceFromHeight(){
+		double DistanceFromHeight = targetHeight - getHeight();
+		return DistanceFromHeight;
 	}
 }
 
--- 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkDualLinearActuator.java
+++ 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/HawkDualLinearActuator.java
@@ -4,28 +4,74 @@ public class HawkDualLinearActuator {
 	
 	HawkActuator motor1;
 	HawkActuator motor2;
+	double speed1;
+	double speed2;
 	
 	public HawkDualLinearActuator(int actuatorID,double actuatorMaxPotValue ,double actuatorMinPotValue, double maxSpeedChange, int limitSwitchForwID, int limitSwitchRevID, int actuatorID2 ,double actuatorMaxPotValue2 ,double actuatorMinPotValue2, double maxSpeedChange2, int limitSwitchForwID2, int limitSwitchRevID2) {
 		motor1 = new HawkActuator(actuatorID, actuatorMaxPotValue , actuatorMinPotValue,  maxSpeedChange, limitSwitchForwID,limitSwitchRevID);
 		motor2 = new HawkActuator(actuatorID2, actuatorMaxPotValue2 , actuatorMinPotValue2,  maxSpeedChange2, limitSwitchForwID2, limitSwitchRevID2);
 	}
 	public void goToHeight(double targetHeight){
-		if(!(motor1.getAnalogInRaw()>motor2.getAnalogInRaw()+2)&& !(motor2.getAnalogInRaw()>motor1.getAnalogInRaw()+2)){
-		motor1.goToHeight(targetHeight);
+		if(!(motor1.getHeight()>motor2.getHeight()+.2)&& !(motor2.getHeight()>motor1.getHeight()+.2)){
 		motor2.goToHeight(targetHeight);
-		}else if(motor1.getAnalogInRaw()>motor2.getAnalogInRaw()+2 && motor1.getSpeed()>=0){
-			motor1.set(.4);
-			motor2.set(.5);
-		}else if(motor2.getAnalogInRaw()>motor1.getAnalogInRaw()+2 && motor2.getSpeed()>=0){
-			motor2.set(.4);
-			motor1.set(.5);
-		}else if(motor1.getAnalogInRaw()<motor2.getAnalogInRaw()-2 && motor1.getSpeed()<=0){
-			motor1.set(-.4);
-			motor2.set(-.5);
-		}else if(motor2.getAnalogInRaw()<motor1.getAnalogInRaw()-2 && motor2.getSpeed()<=0){
-			motor2.set(-.4);
-			motor1.set(-.5);
+		motor1.goToHeight(targetHeight);
+		System.out.println("\n 1");
+	
+		
+		/*}else if(motor1.getHeight()<motor2.getHeight()-.2 && motor1.getCurrentHeight()>targetHeight+.2){
+			motor1.set(-.2);
+			motor2.set(-.3);
+			System.out.println("SOUP");
+		}else if(motor1.getHeight()>motor2.getHeight()+.2 && motor1.getCurrentHeight()>targetHeight+.2){
+			motor1.set(-.3);
+			motor2.set(-.2);
+			System.out.println("soup");
+		}else if(motor1.getHeight()<motor2.getHeight()-.2 && motor1.getCurrentHeight()<targetHeight-.2){
+			motor1.set(.3);
+			motor2.set(.2);
+			System.out.println("SoUp");
+		}else if(motor1.getHeight()>motor2.getHeight()+.2 && motor1.getCurrentHeight()<targetHeight-.2){
+			motor1.set(.2);
+			motor2.set(.3);
+			System.out.println("sOuP");
+		}else{
+			System.out.println("No soup.  3 u");
+			motor1.goToHeight(targetHeight);
+			motor2.goToHeight(targetHeight);*/
+		}else if(motor1.getDistanceFromHeight() > 0){
+			if(motor1.getDistanceFromHeight() > motor2.getDistanceFromHeight() + .2){
+				System.out.println("Soup.");
+				motor1.goToHeight(targetHeight);
+				motor2.sniperToHeight(targetHeight);
+				
+			} else if(motor2.getDistanceFromHeight() > motor1.getDistanceFromHeight() + .2){
+				System.out.println("Stew.");
+				motor1.goToHeight(targetHeight);
+				motor2.sniperToHeight(targetHeight);
+			}
+		}else if(motor1.getDistanceFromHeight() < 0){
+			if(motor1.getDistanceFromHeight() < motor2.getDistanceFromHeight() - .2){
+				System.out.println("Soup.");
+				motor1.goToHeight(targetHeight);
+				motor2.sniperToHeight(targetHeight);
+				
+			} else if(motor2.getDistanceFromHeight() < motor1.getDistanceFromHeight() - .2){
+				System.out.println("Stew.");
+				motor1.goToHeight(targetHeight);
+				motor2.sniperToHeight(targetHeight);
+			}
 		}
+		
+		System.out.println("Height 1 " + motor1.getHeight());
+		System.out.println("Height 2 " + motor2.getHeight());
+
+		System.out.println("Speed" + motor1.getSpeed());
+		System.out.println("Target Height" + targetHeight);
+		System.out.println("Target pot pos: " + motor1.getTargetPotPos());
+		System.out.println("range: "+ motor1.range);
+		System.out.println("Pot pos: "+ motor1.getAnalogInRaw());
+		System.out.println("\n");
+
 	}
 	public void set(double speed){
 		motor1.set(speed);
