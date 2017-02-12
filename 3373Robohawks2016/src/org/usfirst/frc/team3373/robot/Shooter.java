8f8c56d3248ba67fe3eeb6bc0b7a58fd35f9df34
Drew <NetAdmin@STU-US-ROBO2017.internal.highlandschool.org>
2017-02-11 22:44:34 -0500

    updated shooter to reflect voltage not position

--- 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/Shooter.java
+++ 3373Robohawks2016/src/org/usfirst/frc/team3373/robot/Shooter.java
@@ -1,13 +1,35 @@
 package org.usfirst.frc.team3373.robot;
 
+import com.ctre.CANTalon;
 
 public class Shooter {
 CANTalonSafetyNet shooterMotor;
-
+LookupTable lookupTable;
+double[] calibrationVoltages;
+double[] calibrationDistances;
+double distanceToTarget;
+double targetVoltage;
 public Shooter(int shooterPort){
 	shooterMotor = new CANTalonSafetyNet(shooterPort);
+	lookupTable = new LookupTable();
+	calibrationDistances = new double[]{59.5, 71.5, 125, 137.25, 149, 209}; //independent variable
+	calibrationVoltages = new double[]{8.67, 9.1, 10.12, 10.6, 10.6, 11.7};  //dependent variable
+	shooterMotor.changeControlMode(CANTalon.TalonControlMode.Voltage);
+	
+}
+public void determineShooterVoltage(double distance){
+	distanceToTarget = distance;
+	targetVoltage = lookupTable.lookUpValue(distanceToTarget, calibrationDistances, calibrationVoltages);
 }
 
-
+public double getDistanceToTarget(){
+	return distanceToTarget;
+}
+public double getShooterTargetVoltage(){
+	return targetVoltage;
+}
+public void setShooterMotor(double LX){
+	shooterMotor.set(targetVoltage);
+}
 
 }
